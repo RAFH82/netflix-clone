dd285bf6623de79a45b36dc9f31994788f713cca
RAFH82 <hrechkar@gmail.com>
2021-02-09 12:43:42 -0800

    Styling Nav component

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Nav.css";
+import NetflixLogo from "./img/netflix-logo.png";
+import AvatarLogo from "./img/avatar_logo.png";
 
 function Nav() {
-	return <div className="nav"></div>;
+	return (
+		<div className="nav nav__black">
+			<div className="nav__contents">
+				<img className="nav__logo" src={NetflixLogo} alt="" />
+				<img className="nav__avatar" src={AvatarLogo} alt="" />
+			</div>
+		</div>
+	);
 }
 
 export default Nav;
