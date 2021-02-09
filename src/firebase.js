7439228b9151a1c3fcc964543d7bc217631028ff
RAFH82 <hrechkar@gmail.com>
2021-02-08 17:22:48 -0800

    adding firebase cfg

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCuUbErzNufDgwEcAqmlwFRH9VfzHRjRR0",
+	authDomain: "netflix-clone-8bf35.firebaseapp.com",
+	projectId: "netflix-clone-8bf35",
+	storageBucket: "netflix-clone-8bf35.appspot.com",
+	messagingSenderId: "727000591907",
+	appId: "1:727000591907:web:0426d8c001aa83ccd68b96",
+};
