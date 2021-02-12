f8670d1386f5c7fdf0be01970b8c8d30f4864ddf
RAFH82 <hrechkar@gmail.com>
2021-02-11 16:42:40 -0800

    Completed Login Screen component

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.css";
 import loginScreenBackground from "../img/loginScreen_background.png";
+import SignupScreen from "./SignupScreen";
 
 function LoginScreen() {
+	const [signIn, setSignIn] = useState(false);
+
 	return (
 		<div className="loginScreen">
 			<div className="loginScreen__background">
 				<img className="loginScreen__logo" src={loginScreenBackground} alt="" />
-				<button className="loginScreen__button">Sign In</button>
+				<button className="loginScreen__button" onClick={() => setSignIn(true)}>
+					Sign In
+				</button>
 				<div className="loginScreen__gradient" />
+			</div>
+			<div className="loginScreen__body">
+				{signIn ? (
+					<SignupScreen />
+				) : (
+					<>
+						<h1>Unlimited films, TV programs and more!</h1>
+						<h2>Watch anywhere. Cancel at anytime.</h2>
+						<h3>
+							Ready to watch? Enter your email to create or restart your
+							membership
+						</h3>
+						<div className="loginScreen__input">
+							<form>
+								<input type="email" placeholder="Email Address" />
+								<button
+									className="loginScreen__getStarted"
+									onClick={() => setSignIn(true)}
+								>
+									Get started!
+								</button>
+							</form>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
