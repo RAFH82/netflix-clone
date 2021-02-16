a6987810eafd87b93273ae94014d0f93a19a62d6
RAFH82 <hrechkar@gmail.com>
2021-02-16 14:23:19 -0800

    Creating Profile Screen plan component

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -5,6 +5,7 @@ import avatarLogo from "../img/avatar_logo.png";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
+import PlansScreen from "./PlansScreen";
 
 function ProfileScreen() {
 	const user = useSelector(selectUser);
@@ -19,6 +20,7 @@ function ProfileScreen() {
 						<h2>{user.email}</h2>
 						<div className="profileScreen__plans">
 							<h3>Plans</h3>
+							<PlansScreen />
 							<button
 								className="profileScreen__signOut"
 								onClick={() => auth.signOut()}
